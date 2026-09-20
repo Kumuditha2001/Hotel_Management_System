da050550b98018593cdbc366da5b4140092a9ec8
Kumuditha2001 <126276320+Kumuditha2001@users.noreply.github.com>
2026-09-20 23:51:14 +0530

    chages ok running

--- mobile/src/api/api.js
+++ mobile/src/api/api.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your computer's local WiFi IP address + backend port.
 // Your phone and laptop must be on the SAME WiFi network for this to work.
-   const BASE_URL = 'https://1278-2402-d000-8130-2468-5dbd-caeb-83e0-c60c.ngrok-free.app/api';
+     const BASE_URL = 'https://36c8-2402-d000-8130-2852-a125-549a-219f-3bb5.ngrok-free.app/api';
 
    const api = axios.create({
      baseURL: BASE_URL,
